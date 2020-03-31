142a1c3647e3148922e40639f49d9fac88a79ccc
Code Indie <11452791+codeindie@users.noreply.github.com>
2020-03-31 14:59:43 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,36 @@
-self.addEventListener('install', (event) => {
-  console.log('👷', 'install', event);
-  self.skipWaiting();
+const version = "0.1.0";
+const cacheName = `TokoSHTD-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `https://www.smarthafiztalkingdoll.com/`,
+        `https://www.smarthafiztalkingdoll.com/index.html`,
+        `https://www.smarthafiztalkingdoll.com/favicon.ico`,
+        `https://cdn.statically.io/gh/ionic-team/ionicons/master/docs/css/ionicons.min.css`,
+        `https://cdn.statically.io/gh/codeindie/open-project/toko-wa/owlCarousel2-2.3.4/asset/owl.carousel.min.css`,
+        `https://cdn.statically.io/gh/codeindie/open-project/toko-wa/mfp/mfp.min.css`,
+        `https://cdn.statically.io/gh/codeindie/open-project/toko-wa/owlCarousel2-2.3.4/asset/owl.theme.default.min.css`,
+        `https://cdn.staticaly.com/gh/jquery/jquery-dist/master/dist/jquery.min.js`,
+        `https://cdn.statically.io/gh/codeindie/open-project/toko-wa/mfp/mfp.min.js`,
+        `https://cdn.statically.io/gh/codeindie/open-project/toko-wa/owlCarousel2-2.3.4/owl.carousel.min.js`,
+        `https://raw.githubusercontent.com/codeindie/fav-shtd/master/install-me-script.js`,
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('👷', 'activate', event);
-  return self.clients.claim();
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
-  // console.log('👷', 'fetch', event);
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
