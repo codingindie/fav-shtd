fc586abd3a86fbbafa65768fb9ac240a11b38e9c
Code Indie <11452791+codeindie@users.noreply.github.com>
2020-03-31 15:41:42 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,14 @@
 const version = "0.1.1";
 const cacheName = `TokoSHTD-${version}`;
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        `https://www.smarthafiztalkingdoll.com/`,
-        `https://www.smarthafiztalkingdoll.com/index.html`,
-        `https://www.smarthafiztalkingdoll.com/favicon.ico`,
-        `https://cdn.statically.io/gh/ionic-team/ionicons/master/docs/css/ionicons.min.css`,
-        `https://cdn.statically.io/gh/codeindie/open-project/toko-wa/owlCarousel2-2.3.4/asset/owl.carousel.min.css`,
-        `https://cdn.statically.io/gh/codeindie/open-project/toko-wa/mfp/mfp.min.css`,
-        `https://cdn.statically.io/gh/codeindie/open-project/toko-wa/owlCarousel2-2.3.4/asset/owl.theme.default.min.css`,
-        `https://cdn.staticaly.com/gh/jquery/jquery-dist/master/dist/jquery.min.js`,
-        `https://cdn.statically.io/gh/codeindie/open-project/toko-wa/mfp/mfp.min.js`,
-        `https://cdn.statically.io/gh/codeindie/open-project/toko-wa/owlCarousel2-2.3.4/owl.carousel.min.js`,
-        `https://raw.githubusercontent.com/codeindie/fav-shtd/master/install-me-script.js`,
-      ])
-          .then(() => self.skipWaiting());
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          'https://www.smarthafiztalkingdoll.com/',
+          'https://www.smarthafiztalkingdoll.com/index.html',
+        ]
+      );
     })
   );
 });
@@ -34,15 +26,3 @@ self.addEventListener('fetch', event => {
     })
   );
 });
-
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-
-importScripts(
-  "/static/precache.f9cac6e832615acd35339c53b8d34f39.js"
-);
-
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|svg)$/, workbox.strategies.cacheFirst(), 'GET');
